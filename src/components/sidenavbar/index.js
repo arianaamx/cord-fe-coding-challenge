f3d2b2829c36c2d4bb9dc5e7f90aeceba6809238
Ariana <ariana.milasincic@gmail.com>
2021-02-19 15:59:27 +0100

    Add styling to SideNavBar

--- src/components/sidenavbar/index.js
+++ src/components/sidenavbar/index.js
@@ -10,18 +10,24 @@ import styled, { css } from "styled-components";
 function SideNavBar() {
   const [activeSideBar, setActiveSideBar] = useState(true);
 
+  const handleMenuClose = () => {
+    setActiveSideBar(!activeSideBar);
+  };
+
   return (
     <SideNavBarCont isVisible={activeSideBar}>
       {/* Implement a hamburger icon slide in effect for mobile devices */}
-      <SideNavMainLink className="menu_nav_link main_nav_link" to="/" activeClassName="active" exact>
+      <SideNavMainLink mainlink={+true} className="menu_nav_link" to="/" activeClassName="active" exact>
         Wesley
-        <NavIcon arrow>
+        <NavIcon arrow onClick={handleMenuClose}>
           <img src={Arrow} alt="menuArrow" />
         </NavIcon>
       </SideNavMainLink>
-      <SideNavMainLink className="menu_nav_link" to="/discover" activeClassName="active">
+      <SideNavMainLink discoverlink={+true} className="menu_nav_link" to="/discover" activeClassName="active">
         Discover
-        <NavIcon search></NavIcon>
+        <NavIcon search>
+          <img src={SearchWhite} alt="menuSearch" />
+        </NavIcon>
       </SideNavMainLink>
       <SideNavHeader>
         <HeaderText>Watched</HeaderText>
@@ -39,7 +45,7 @@ function SideNavBar() {
         Movies
       </NavLink>
       <NavLink className="menu_nav_link" to="/saved/tv-shows" activeClassName="active">
-        Tv Shows
+        TV Shows
       </NavLink>
     </SideNavBarCont>
   );
@@ -50,12 +56,12 @@ export default SideNavBar;
 const SideNavBarCont = styled.div`
   position: fixed;
   z-index: 9;
-  width: 280px;
+  width: 260px;
   height: 100%;
-  background-color: ${colors.sideNavBar}
-    ${(props) =>
-      !props.isVisible &&
-      `
+  background-color: ${colors.sideNavBar};
+  ${(props) =>
+    !props.isVisible &&
+    `
     display:none
     `};
 `;
@@ -66,9 +72,11 @@ const SideNavMainLink = styled(Link)`
   justify-content: space-between;
   align-items: center;
   padding: 25px 35px;
-  font-size: 1.6em;
-  font-weight: 700;
+  padding-top: ${(props) => (props.mainlink ? "35px" : "25px")};
+  font-size: 1.4em;
+  font-weight: ${(props) => (props.mainlink ? "700" : "300")};
   color: white;
+  background-color: ${(props) => (props.discoverlink ? colors.primaryColor : colors.sideNavBar)};
 `;
 
 const NavIcon = styled.div`
@@ -76,10 +84,23 @@ const NavIcon = styled.div`
   right: 35px;
 `;
 
-const SideNavHeader = styled.div``;
+const SideNavHeader = styled.div`
+  padding: 35px 35px;
+  padding-bottom: 15px;
+  margin-bottom: 15px;
+  box-shadow: 35px 1px 1px -1px ${colors.lightBackground};
+`;
 
-const HeaderText = styled.div``;
+const HeaderText = styled.div`
+  font-size: 1.4em;
+  font-weight: 400;
+  color: white;
+`;
 
 const NavLink = styled(Link)`
   display: block;
+  padding-left: 35px;
+  color: white;
+  font-weight: 300;
+  padding-bottom: 10px;
 `;
