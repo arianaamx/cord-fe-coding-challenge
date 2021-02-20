f750e2011dafd9664804d7364e566aab702deee5
Ariana <ariana.milasincic@gmail.com>
2021-02-20 19:26:30 +0100

    Add npm packages: react-icons and react-responsive

--- src/components/sidenavbar/index.js
+++ src/components/sidenavbar/index.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import { NavLink as Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 import * as colors from "../../colors";
 import Arrow from "../../images/arrow-icon.png";
 import SearchWhite from "../../images/search-icon-white.png";
 
 import styled, { css } from "styled-components";
+
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return isMobile ? children : null;
+};
+const Default = ({ children }) => {
+  const isNotMobile = useMediaQuery({ minWidth: 768 });
+  return isNotMobile ? children : null;
+};
 
 function SideNavBar() {
   const [activeSideBar, setActiveSideBar] = useState(true);
@@ -15,39 +25,43 @@ function SideNavBar() {
   };
 
   return (
-    <SideNavBarCont isVisible={activeSideBar}>
-      {/* Implement a hamburger icon slide in effect for mobile devices */}
-      <SideNavMainLink mainlink={+true} className="menu_nav_link" to="/" activeClassName="active" exact>
-        Wesley
-        <NavIcon arrow onClick={handleMenuClose}>
-          <img src={Arrow} alt="menuArrow" />
-        </NavIcon>
-      </SideNavMainLink>
-      <SideNavMainLink discoverlink={+true} className="menu_nav_link" to="/discover" activeClassName="active">
-        Discover
-        <NavIcon search>
-          <img src={SearchWhite} alt="menuSearch" />
-        </NavIcon>
-      </SideNavMainLink>
-      <SideNavHeader>
-        <HeaderText>Watched</HeaderText>
-      </SideNavHeader>
-      <NavLink className="menu_nav_link" to="/watched/movies" activeClassName="active">
-        Movies
-      </NavLink>
-      <NavLink className="menu_nav_link" to="/watched/tv-shows" activeClassName="active">
-        TV Shows
-      </NavLink>
-      <SideNavHeader>
-        <HeaderText>Saved</HeaderText>
-      </SideNavHeader>
-      <NavLink className="menu_nav_link" to="/saved/movies" activeClassName="active">
-        Movies
-      </NavLink>
-      <NavLink className="menu_nav_link" to="/saved/tv-shows" activeClassName="active">
-        TV Shows
-      </NavLink>
-    </SideNavBarCont>
+    <>
+      <Mobile>{/* Implement a hamburger icon slide in effect for mobile devices */}Mobile</Mobile>
+      <Default>
+        <SideNavBarCont isVisible={activeSideBar}>
+          <SideNavMainLink mainlink={+true} className="menu_nav_link" to="/" activeClassName="active" exact>
+            Wesley
+            <NavIcon arrow onClick={handleMenuClose}>
+              <img src={Arrow} alt="menuArrow" />
+            </NavIcon>
+          </SideNavMainLink>
+          <SideNavMainLink discoverlink={+true} className="menu_nav_link" to="/discover" activeClassName="active">
+            Discover
+            <NavIcon search>
+              <img src={SearchWhite} alt="menuSearch" />
+            </NavIcon>
+          </SideNavMainLink>
+          <SideNavHeader>
+            <HeaderText>Watched</HeaderText>
+          </SideNavHeader>
+          <NavLink className="menu_nav_link" to="/watched/movies" activeClassName="active">
+            Movies
+          </NavLink>
+          <NavLink className="menu_nav_link" to="/watched/tv-shows" activeClassName="active">
+            TV Shows
+          </NavLink>
+          <SideNavHeader>
+            <HeaderText>Saved</HeaderText>
+          </SideNavHeader>
+          <NavLink className="menu_nav_link" to="/saved/movies" activeClassName="active">
+            Movies
+          </NavLink>
+          <NavLink className="menu_nav_link" to="/saved/tv-shows" activeClassName="active">
+            TV Shows
+          </NavLink>
+        </SideNavBarCont>
+      </Default>
+    </>
   );
 }
 
