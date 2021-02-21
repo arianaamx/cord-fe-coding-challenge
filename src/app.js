0318e9679d3f55e12517e8974703930569d18759
Ariana <ariana.milasincic@gmail.com>
2021-02-21 23:06:26 +0100

    Change css styled-components

--- src/app.js
+++ src/app.js
@@ -1,34 +1,40 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import styled from "styled-components";
+import { useMediaQuery } from "react-responsive";
+import styled, { css } from "styled-components";
 
-import Menu from "./pages/home";
+import Home from "./pages/home";
 
 import Discover from "./pages/discover";
 
 import "./css/app.css";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <PageContainer>
-          <Menu />
-          <ContentWrapper>
-            <Switch>
-              <Route path="/discover" component={Discover} {...this.props} />
-            </Switch>
-          </ContentWrapper>
-        </PageContainer>
-      </Router>
-    );
-  }
+export default function App() {
+  const isNotMobile = useMediaQuery({ minWidth: 768 });
+
+  return (
+    <Router>
+      <PageContainer>
+        <Home />
+        <ContentWrapper isNotMobile={isNotMobile}>
+          <Switch>
+            <Route path="/discover" component={Discover} />
+          </Switch>
+        </ContentWrapper>
+      </PageContainer>
+    </Router>
+  );
 }
 
-const ContentWrapper = styled.main`
-  padding-left: 280px;
+const ContentWrapper = styled.div`
+  ${(props) =>
+    props.isNotMobile
+      ? css`
+          padding-left: 280px;
+        `
+      : css`
+          padding: 0;
+        `}
 `;
 
-const PageContainer = styled.main`
-  overflow-x: hidden;
-`;
+const PageContainer = styled.div``;
