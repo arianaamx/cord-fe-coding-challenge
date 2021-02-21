100a631b039ee58f1b7b7a3fa7272444fd8c6d21
Ariana <ariana.milasincic@gmail.com>
2021-02-21 02:11:29 +0100

    Add API requests: getPopular and getGenres

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,7 +1,33 @@
-import axios from 'axios';
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_THE_MOVIE_DB_API_KEY;
 
 // All of your API requests should be in this file
 // i.e.
 // export const getMovieGenres = async () => {
 
 // };
+
+export const getPopularMovies = async (setPopularMovies) => {
+  await axios
+    .get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+    .then((response) => {
+      console.log(response);
+      setPopularMovies(response.data.results);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const getMovieGenres = async (setGenreOptions) => {
+  await axios
+    .get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`)
+    .then((response) => {
+      console.log(response);
+      setGenreOptions(response.data.genres);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
