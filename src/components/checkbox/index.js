420e3308f3b0fd0a79adf946e543d85ccd5acaa3
Ariana <ariana.milasincic@gmail.com>
2021-02-22 13:49:30 +0100

    Create Checkbox component

--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -1,17 +1,33 @@
-import React from "react";
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-export default class CheckBox extends React.Component {
+export default function CheckBox({ isClicked, label, id }) {
   // Create a custom checkbox component
-  
-  render () {
-    return (
-      <CheckboxCont>
-      </CheckboxCont>
-    )
-  }
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleInputChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <CheckboxCont>
+      <InputCheck type="checkbox" checked={isChecked} onChange={handleInputChange} id={`${id}`} />
+      <label className="checbox-label" htmlFor={`${id}`}>
+        {label}
+      </label>
+    </CheckboxCont>
+  );
 }
 
 const CheckboxCont = styled.div`
   position: relative;
-`
+
+  .checbox-label {
+    padding-left: 10px;
+  }
+`;
+
+const InputCheck = styled.input`
+  width: 15px;
+  height: 15px;
+`;
