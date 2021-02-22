13fe77bf52d6bbaffea88049972acc7a61889219
Ariana <ariana.milasincic@gmail.com>
2021-02-22 13:49:50 +0100

    Add Checkbox component and styling

--- src/components/expandablefilters/index.js
+++ src/components/expandablefilters/index.js
@@ -1,16 +1,59 @@
-import React from "react";
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 import Checkbox from "../../components/checkbox";
 
-export default class ExpandableFilters extends React.Component {
-  constructor (props) {
-    super(props);
+export default function ExpandableFilters({ options, title }) {
+  const [filtersShown, setFilterShown] = useState(false);
 
-    this.state = {
-      filtersShown: false
-    };
-  }
+  const handleOnClickChange = () => {
+    setFilterShown(!filtersShown);
+  };
+
+  return (
+    <ExpandableFiltersWrapper>
+      <ExpandableFiltersTitleCont>
+        {filtersShown ? (
+          <AiOutlineMinus onClick={handleOnClickChange} size={"1.3em"} />
+        ) : (
+          <AiOutlinePlus onClick={handleOnClickChange} size={"1.3em"} />
+        )}
+        <ExpandableFiltersTitle> Select {title}</ExpandableFiltersTitle>
+      </ExpandableFiltersTitleCont>
+      <ExpandableFiltersOptions isOpen={filtersShown}>
+        {options.map((element) => {
+          return (
+            <OptionsChecboxWrapper>
+              <Checkbox key={element.id} label={element.name} id={element.id} />
+            </OptionsChecboxWrapper>
+          );
+        })}
+      </ExpandableFiltersOptions>
+    </ExpandableFiltersWrapper>
+  );
 
   // You need to create your own checkbox component with a custom checkmark
 }
+
+const ExpandableFiltersWrapper = styled.div`
+  padding-bottom: 15px;
+`;
+
+const ExpandableFiltersTitleCont = styled.div`
+  display: flex;
+  font-weight: 700;
+`;
+
+const ExpandableFiltersTitle = styled.span`
+  padding-left: 10px;
+`;
+
+const ExpandableFiltersOptions = styled.div`
+  ${(props) => (props.isOpen ? "" : `display:none`)}
+  padding-top: 20px;
+`;
+
+const OptionsChecboxWrapper = styled.div`
+  padding-bottom: 10px;
+`;
