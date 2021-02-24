5e853945c4a67ac6e29a5f2e9574d1770eb41820
Ariana <ariana.milasincic@gmail.com>
2021-02-24 04:09:50 +0100

    Updates

---
+++ precache-manifest.3f777dcb7053ab16ad4473466777563e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aff988da4ad4386d333f08f69187fe1",
+    "url": "/cord-fe-coding-challenge/index.html"
+  },
+  {
+    "revision": "d62d51885e75ef498f7d",
+    "url": "/cord-fe-coding-challenge/static/css/main.df26aff6.chunk.css"
+  },
+  {
+    "revision": "950b401b0a7e84c2e9f1",
+    "url": "/cord-fe-coding-challenge/static/js/2.41a18fab.chunk.js"
+  },
+  {
+    "revision": "d62d51885e75ef498f7d",
+    "url": "/cord-fe-coding-challenge/static/js/main.879e2237.chunk.js"
+  },
+  {
+    "revision": "66dc14d3bbb1b394f6b0",
+    "url": "/cord-fe-coding-challenge/static/js/runtime~main.b93949bb.js"
+  }
+]);
